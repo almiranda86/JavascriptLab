8ee1ccbedeaea50e22e9ceb873bc9c656d97312e
Andre Luis Miranda <almiranda@brq.com>
2019-12-06 09:46:57 -0300

    Functions pt2

--- Functions2/js/functions.js
+++ Functions2/js/functions.js
@@ -1,23 +1,23 @@
-// function globalThis(){
-//     console.log(this);
-// }
+function globalThis(){
+    console.log(this);
+}
 
 
-// //new object
-// function objectTest(){
-//     this.a = 'a'
-//     this.b = 'b'
-//     this.c = 'c'
-//     this.metodo = function(){
-//         return 'd';
-//     }
-// }
+//new object
+function objectTest(){
+    this.a = 'a'
+    this.b = 'b'
+    this.c = 'c'
+    this.metodo = function(){
+        return 'd';
+    }
+}
 
-// function newObject(){
-//     //console.log(new objectTest());
-//     var objecttest = new objectTest();
-//     console.log(objecttest);
-// }
+function newObject(){
+    //console.log(new objectTest());
+    var objecttest = new objectTest();
+    console.log(objecttest);
+}
 
 
 
@@ -48,3 +48,44 @@ function usingApply(p1, p2, p3){
 }
 
 usingApply.apply(varAplly, ['abadaba', 123, [5,6,7]]);
+
+
+
+//bind
+//
+var varBind={
+    nome: 'Bind'
+}
+
+function usingBind(p1, p2, p3){
+    console.log(`p1: ${p1}, p2: ${p2}, p3: ${p3}`);
+    console.log('varBind: ', this);
+}
+
+var bindFn = usingBind.bind(varBind, 'A1', 'B1');
+bindFn('C1');
+
+
+
+//Arrow function
+//
+
+var usingArrowFunction = () => {
+    return console.log(`Arrow Function ${soma(2, 5)}`);
+}
+
+var soma = (a, b) => {
+    return a + b;
+}
+
+console.log(`Arrow Function: ${soma(1,2)}`);
+
+var retornaObjeto = (p3) => (
+    {
+        p1: 'P1',
+        p2: 'P2',
+        p3
+    }
+)
+
+console.log(retornaObjeto('p3'));
