434bf5b88bcfee87751c8de8a28f90428b1867c5
Andre Luis Miranda <almiranda@brq.com>
2019-12-16 12:27:14 -0300

    Hoisting e algum refactor

---
+++ Hoisting/js/hoisting.js
@@ -0,0 +1,16 @@
+// console.log(a);
+// var a = 123;
+
+
+// console.log(b);
+// let b = 11;
+
+// console.log(soma(10,5));
+// var soma =  function(x, y){
+//     return x + y;
+// }
+
+console.log(soma(10,5));
+function soma(x,y){
+    return x + y;
+}
