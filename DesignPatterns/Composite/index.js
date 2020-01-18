d04bc5d262d35edcb81cec86c984ec35a06e8af7
Andre Luis Miranda <almiranda86@gmail.com>
2020-01-18 17:44:51 -0300

    Composite - ver2 Implementacao do padrao

--- DesignPatterns/Composite/index.js
+++ DesignPatterns/Composite/index.js
@@ -1,10 +1,28 @@
 var CatalogItem = require('./CatalogItem');
+var CatalogGroup = require('./CatalogGroup');
 
 var boots = new CatalogItem('botas de couro', 80);
 var tenis = new CatalogItem('adidas', 200);
 var chinelo = new CatalogItem('havaianas', 50);
 
-console.log('total botas: ', `$${boots.total}`);
+var grupo = new CatalogGroup('Tenis e mais', [boots, tenis, chinelo]);
 
-boots.print();
-tenis.print();
+var grupo_comida = new CatalogGroup('Comida', [
+    new CatalogItem('milkshake', 10),
+    new CatalogItem('batata fria', 10),
+]);
+
+var keychain = new CatalogItem('Key Chain', .50);
+
+// console.log('total botas: ', `$${boots.total}`);
+// console.log('total tenis: ', `$${grupo.total}`);
+
+// boots.print();
+// tenis.print();
+
+// grupo.print();
+
+var catalog = new CatalogGroup('Comida e Roupa', [keychain, grupo, grupo_comida]);
+
+console.log(`$${catalog.total}`);
+catalog.print();
