b9440e5b8adc802cd33ed749cdfa4225f16128d6
Andre Luis Miranda <almiranda86@gmail.com>
2020-01-18 01:49:13 -0300

    Adapter - ver2 Implementacao do padrao

--- DesignPatterns/Adapter/index.js
+++ DesignPatterns/Adapter/index.js
@@ -1,11 +1,15 @@
+var localStorage = require('./localStorage');
+
 console.log('local storage len: ', localStorage.length);
+
+localStorage.makeEmployee();
 
 var uid = localStorage.getItem('user_id');
 
 console.log('user_id: ', uid);
 
 if(!uid){
-    console.log('Usuario nao encontrado. Configurando usuario e toke...');
+    console.log('Usuario nao encontrado. Configurando usuario e token...');
     localStorage.setItem('token','45SD465D4FS5S45FSSDS54');
     localStorage.setItem('user_id', '12345')
 }else{
