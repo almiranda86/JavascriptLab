ebb656643468b0e94785948367aedf69d0a787e4
Andre Luis Miranda <almiranda86@gmail.com>
2020-01-18 01:57:45 -0300

    Proxy - ver1 Implementacao inicial

---
+++ DesignPatterns/Proxy/README.md
@@ -0,0 +1,5 @@
+PROXY
+
+Forneça um substituto ou espaço reservado para outro objeto para controlar o acesso a ele.
+
+Provide a surrogate or placeholder for another object to control access to it.
---
+++ DesignPatterns/Proxy/index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var path = require('path');
+
+var txtFile = path.join(__dirname, 'text.txt');
+var mdFile = path.join(__dirname, 'text.md');
+
+var result = (error, contents) => {
+    if(error){
+        console.log('\x07');
+        console.log(error);
+        process.exit(0);
+    }
+
+    console.log('Lendo arquivo...');
+    console.log(contents);
+}
+
+fs.readFile(txtFile,'UTF-8', result);
+fs.readFile(mdFile,'UTF-8', result);
