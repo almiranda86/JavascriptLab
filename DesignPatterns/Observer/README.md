7d9f1e5fa38d7d26111fb2408e8c7ea14cb4cb3b
Andre Luis Miranda <almiranda86@gmail.com>
2020-01-19 01:15:34 -0300

    Observer - ver1 Implementacao inicial

---
+++ DesignPatterns/Observer/README.md
@@ -0,0 +1,5 @@
+OBSERVER
+
+Defina uma dependência de um para muitos entre objetos para que, quando um objeto mudar de estado, todos os seus dependentes sejam notificados e atualizados automaticamente.
+
+Define a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically.
---
+++ DesignPatterns/Observer/index.js
@@ -0,0 +1,14 @@
+var Store = require('./Store');
+var Shopper = require('./Shopper');
+var Mall = require('./Mall');
+
+var catsAndThings = new Store("Cats & Things");
+var insAndOuts = new Store("Ins and Outs");
+
+var alex = new Shopper("Alex");
+var eve = new Shopper("Eve");
+var sharon = new Shopper("Sharon");
+var mike = new Shopper("Mike");
+
+var valleyMall = new Mall();
+
